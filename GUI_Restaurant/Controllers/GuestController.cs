064f9eb24f43cc2a5bcd64666b6e84b47a19877a
eengstroem <59916483+eengstroem@users.noreply.github.com>
2021-04-20 19:06:06 +0200

    final commity

---
+++ GUI_Restaurant/Controllers/GuestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GUI_Restaurant.Models;
+
+namespace GUI_Restaurant.Controllers
+{
+    public class GuestController : Controller
+    {
+        private RestaurantDbContext db = new();
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            var guests = db.Guests.ToList();
+            return View(guests);
+        }
+
+        [HttpGet]
+        [Route("create")]
+            
+    }
+}
