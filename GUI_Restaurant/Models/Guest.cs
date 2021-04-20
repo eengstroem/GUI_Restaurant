695bbd9e05739843934e98a915da41a3a43a32a5
eengstroem <59916483+eengstroem@users.noreply.github.com>
2021-04-20 15:19:48 +0200

    added a Guest and started working on DbContext

---
+++ GUI_Restaurant/Models/Guest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GUI_Restaurant.Models
+{
+    public class Guest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime date { get; set; }
+    }
+}
---
+++ GUI_Restaurant/Models/RestaurantDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GUI_Restaurant.Models
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public virtual DbSet<Guest> Guests { get; set; }
+
+        public RestaurantDbContext() : base()
+        {
+
+        }
+    }
+}
